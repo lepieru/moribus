dd855f02a35939a7374d6940be0cd39d2b44ef90
Pierre Le Gall <pierre@legall.io>
2016-01-29 12:56:25 +0100

    fabrique.py cleaned-up

--- src/fabrique.py
+++ src/fabrique.py
@@ -10,34 +10,29 @@ class Fabrique:
         self.monde = le_monde
 
     def fabriquer(self):
-        le_sol = visu.Objet(maillage=visu.Sol())
-        self.monde.ajouter(decor=le_sol)
+        sol = visu.Objet(maillage=visu.Sol())
 
-        le_tableau = visu.Objet(maillage=visu.Tableau(recto="data/textures/gris.jpg",
-                                                      verso="data/textures/Ceramic.jpg",
-                                                      largeur=2.0, hauteur=3.0, epaisseur=0.1))
-        le_tableau.placer(geo.Vec3((0.0, 0.0, 2.0)))
-        self.monde.ajouter(decor=le_tableau)
+        arbre = visu.Objet(maillage=visu.Panneau(recto="data/textures/tree1.png",
+                                                 verso="data/textures/tree1.png",
+                                                 largeur=6.0, hauteur=12.0, epaisseur=0.1))
+        arbre.placer(geo.Vec3((5.0, 5.0, 0.0)))
 
-        le_tableau = visu.Objet(maillage=visu.Panneau(recto="data/textures/tree1.png",
-                                                      verso="data/textures/tree1.png",
-                                                      largeur=6.0, hauteur=12.0, epaisseur=0.1))
-        le_tableau.placer(geo.Vec3((5.0, 5.0, 0.0)))
-        self.monde.ajouter(decor=le_tableau)
+        pingouin = visu.Objet(maillage=visu.Obj(url="data/obj/pingouin/p.obj"))
+        pingouin.placer(geo.Vec3((-1.0, 0.0, 0.0)))
+        pingouin.orienter(45.0 * math.pi / 180.0)
 
-        le_pingouin = visu.Objet(maillage=visu.Obj(
-            url="data/obj/pingouin/p.obj"))
-        le_pingouin.placer(geo.Vec3((-1.0, 0.0, 0.0)))
-        le_pingouin.orienter(45.0 * math.pi / 180.0)
-        self.monde.ajouter(decor=le_pingouin)
+        activite_effraye = simu.Effaye(id="effraye", objet=pingouin, monde=self.monde)
+        activite_aggressif = simu.Aggressif(id="aggressif", objet=pingouin, monde=self.monde)
+        activite_curieux = simu.Curieux(id="curieux", objet=pingouin, monde=self.monde)
+        activite_pose = simu.Pose(id="pose", objet=pingouin, monde=self.monde)
 
-        une_activite = simu.Effaye(id="effraye", objet=le_pingouin, monde=self.monde)
-        self.monde.ajouter(activite=une_activite)
-        une_activite = simu.Aggressif(id="aggressif", objet=le_pingouin, monde=self.monde)
-        self.monde.ajouter(activite=une_activite)
-        une_activite = simu.Curieux(id="curieux", objet=le_pingouin, monde=self.monde)
-        self.monde.ajouter(activite=une_activite)
-        une_activite = simu.Pose(id="pose", objet=le_pingouin, monde=self.monde)
-        self.monde.ajouter(activite=une_activite)
+        self.monde.ajouter(decor=sol)
+        self.monde.ajouter(decor=arbre)
+        self.monde.ajouter(decor=pingouin)
 
-        self.monde.pingouin = le_pingouin
+        self.monde.ajouter(activite=activite_effraye)
+        self.monde.ajouter(activite=activite_aggressif)
+        self.monde.ajouter(activite=activite_curieux)
+        self.monde.ajouter(activite=activite_pose)
+
+        self.monde.pingouin = pingouin
